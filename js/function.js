3544f6a84d565bcb67441959d5327afb417c91a9
unknown <mscigliano33@gmail.com>
2023-10-01 19:02:02 -0300

    guardado final en repositorio

---
+++ js/function.js
@@ -0,0 +1,6 @@
+
+document.getElementById('enviar').addEventListener('click', function () {
+    console.log ("confirmacion de envio");
+    document.getElementById("indicador").innerHTML = "Gracias, pronto me contactare con Ud.";
+    document.body.style.backgroundColor = 'lightgreen'
+})
